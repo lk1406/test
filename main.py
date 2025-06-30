1dc3fd5e746683c3f3c1cfef58dec29eb51c557e
lk1406 <144722112+lk1406@users.noreply.github.com>
2025-06-30 19:24:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,5 +10,13 @@ def receive_command():
     # Here you would run your LLM + LangGraph
     return jsonify({"status": "received", "command": command})
 
+
+@app.route('/', methods=['GET'])
+def health():
+    print("All ok")
+    # Here you would run your LLM + LangGraph
+    return jsonify({"status": "working"})
+
+
 if __name__ == '__main__':
     app.run(port=5000)
